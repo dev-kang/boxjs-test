72b70ce6ff2d62cc126b57a67df06bbf1d40a3a4
DevKang <cn.devkang@gmail.com>
2023-02-20 10:36:24 +0800

    Update sample-rewrite-with-script.js

--- sample-rewrite-with-script.js
+++ sample-rewrite-with-script.js
@@ -17,11 +17,11 @@
 // You can optional change the response headers at the same time by using $done({body: modifiedBody, headers: modifiedHeaders}); only change the response headers is not allowed for script-response-body. The modifiedHeaders can be copied and modified from $response.headers, please do not change the content length, type and encoding field.
 // Response status can also be optional changed by using $done({body: modifiedBody, headers: modifiedHeaders, status: modifiedStatus}), the modifiedStatus should be like "HTTP/1.1 200 OK"
 
-var body = $response.body;
-var obj = JSON.parse(body);
+// var body = $response.body;
+// var obj = JSON.parse(body);
 
-obj['result'] = 0;
-body = JSON.stringify(obj);
+// obj['result'] = 0;
+body = 1;
 
 console.log(body);
 
